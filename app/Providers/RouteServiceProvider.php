b7460f1a16d7d63eaff6f65ab55af14438e95be2
Sahakyan88 <onik.sahakyan.99@list.ru>
2022-11-22 21:36:14 +0400

    new changes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,31 +2,24 @@
 
 namespace App\Providers;
 
-use Illuminate\Cache\RateLimiting\Limit;
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use App;
+use Illuminate\Cache\RateLimiting\Limit;
+
+use Illuminate\Support\Facades\RateLimiter;
+
 
 class RouteServiceProvider extends ServiceProvider
 {
     /**
-     * The path to the "home" route for your application.
+     * This namespace is applied to your controller routes.
      *
-     * This is used by Laravel authentication to redirect users after login.
+     * In addition, it is set as the URL generator's root namespace.
      *
      * @var string
      */
-    public const HOME = '/dashboard';
-
-    /**
-     * The controller namespace for the application.
-     *
-     * When present, controller route declarations will automatically be prefixed with this namespace.
-     *
-     * @var string|null
-     */
-    protected $namespace = 'App\\Http\\Controllers';
+    protected $namespace = 'App\Http\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -35,31 +28,89 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+       
+        
+        parent::boot();
         $this->configureRateLimiting();
-
-        $this->routes(function () {
-            Route::prefix('api')
-                ->middleware('api')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/api.php'));
-                
-                Route::prefix('auth')
-                ->middleware('guest')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/auth.php'));
-
-            Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/web.php'));
-            
-            Route::middleware('admin')
-                ->prefix('admin')
-                ->namespace($this->namespace.'\Admin')
-                ->group(base_path('routes/admin.php'));
-        });
     }
 
     /**
+     * Define the routes for the application.
+     *
+     * @return void
+     */
+    public function map()
+    {
+        // $this->mapApiRoutes();
+
+        $this->mapAdminRoutes();
+        
+        $this->mapWebRoutes();
+
+    }
+
+    /**
+     * Define the "web" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @return void
+     */
+    protected function mapWebRoutes()
+    {
+        // Localization
+        $request = app(\Illuminate\Http\Request::class);
+        $locale = $request->segment(1);
+
+        if($locale == 'am'){
+            header("Location: /");
+            exit();
+        }
+        // TODO ACTIVE LANG
+        $activeLangs = array('am','en','ru');
+        
+        if ( ! in_array($locale, $activeLangs)) {
+            App::setLocale(config('app.fallback_locale'));
+        } else {
+            App::setLocale($locale);
+        }
+        // Localization
+        
+        Route::middleware('web')
+             ->namespace($this->namespace)
+             ->group(base_path('routes/web.php'));
+    }
+
+    /**
+     * Define the "api" routes for the application.
+     *
+     * These routes are typically stateless.
+     *
+     * @return void
+     */
+    protected function mapApiRoutes()
+    {
+        Route::prefix('api')
+             ->middleware('api')
+             ->namespace($this->namespace)
+             ->group(base_path('routes/api.php'));
+    }
+
+     /**
+     * Define the "admin" routes for the application.
+     *
+     * These routes are typically stateless.
+     *
+     * @return void
+     */
+    protected function mapAdminRoutes()
+    {
+        Route::prefix('admin')
+             ->middleware('admin')
+             ->namespace('App\Http\Controllers\Admin')
+             ->group(base_path('routes/admin.php'));
+    }
+      /**
      * Configure the rate limiters for the application.
      *
      * @return void
