b4744fce48b5678dd513944a14881f5119188406
ElenPanoyan <elen.panoyan198@gmail.com>
2022-12-17 12:57:40 +0200

    Title changes

--- resources/views/admin/package/package.blade.php
+++ resources/views/admin/package/package.blade.php
@@ -34,12 +34,12 @@
                         <thead>
                             <tr>
                                 <th>ID</th>
-                                <th>F.Name</th>
-                                <th>L.Name</th>
+                                <th>Name</th>
+                                <th>Surname</th>
                                 <th>Registered</th>
                                 <th>Phone</th>
-                                <th>Service</th>
-                                <th>AMD</th>
+                                <th>Package</th>
+                                <th>Price</th>
                                 <th>Created</th>
                                 <th>Status</th>
                             </tr>
@@ -47,12 +47,12 @@
                         <tfoot>
                             <tr>
                                 <th>ID</th>
-                                <th>F.Name</th>
-                                <th>L.Name</th>
+                                <th>Name</th>
+                                <th>Surname</th>
                                 <th>Registered</th>
                                 <th>Phone</th>
-                                <th>Service</th>
-                                <th>AMD</th>
+                                <th>Package</th>
+                                <th>Price</th>
                                 <th>Created</th>
                                 <th>Status</th>
                         </tfoot>
@@ -62,7 +62,7 @@
             </div>
         </div>
     </div>
-    <!-- Main page content-->    
+    <!-- Main page content-->
 </main>
 @push('css')
     <link href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
@@ -71,21 +71,21 @@
     <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
     <script>
-      
+
         $(document).ready(function() {
             const capitalize = (s) => {
                 if (typeof s !== 'string') return ''
                 return s.charAt(0).toUpperCase() + s.slice(1)
             }
             var dataTable =  $('#dataTable').DataTable({
-                
+
                 "processing": true,
                 "serverSide": true,
                 'searching': true,
                 "ajax": {
                     "url": "{{ route('aPackagesData') }}",
                     "data": function(data){
-                     
+
                         data['sort_field'] = data.columns[data.order[0].column].name;
                         data['sort_dir'] =  data.order[0].dir;
                         data['search'] = data.search.value;
@@ -95,17 +95,17 @@
 
                         var filter_status = $('#filter_status').val();
                         data.filter_status = filter_status;
-                        
+
                         var filter_verification = $('#filter_verification').val();
                         data.filter_verification = filter_verification;
                     }
-                   
+
                 },
                 "fnDrawCallback": function( oSettings ) {
                     feather.replace();
                     $('[data-toggle="popover"]').popover();
                 },
-                
+
                 "columns": [
                     { "data": "id", "name":'id', "orderable": true },
                     { "data": "first_name", "name":'first_name', "orderable": true },
@@ -169,7 +169,7 @@
                 class: 'modal',
                 minHeight: '200',
             })
-            
+
         });
     </script>
 @endpush
--- resources/views/admin/payments/oneTimePayment.blade.php
+++ resources/views/admin/payments/oneTimePayment.blade.php
@@ -33,26 +33,26 @@
                         <thead>
                             <tr>
                                 <th>ID</th>
-                                <th>F.Name</th>
-                                <th>L.Name</th>
+                                <th>Name</th>
+                                <th>Surname</th>
                                 <th>Phone</th>
                                 <th>Service</th>
-                                <th>AMD</th>
+                                <th>Price</th>
                                 <th>Date</th>
-                                <th>status_been</th>
+                                <th>Status</th>
                                 <th>Edit</th>
                             </tr>
                         </thead>
                         <tfoot>
                             <tr>
                                 <th>ID</th>
-                                <th>F.Name</th>
-                                <th>L.Name</th>
+                                <th>Name</th>
+                                <th>Surname</th>
                                 <th>Phone</th>
                                 <th>Service</th>
-                                <th>AMD</th>
+                                <th>Price</th>
                                 <th>Date</th>
-                                <th>status_been</th>
+                                <th>Status</th>
                                 <th>Edit</th>
                             </tr>
                         </tfoot>
@@ -62,7 +62,7 @@
             </div>
         </div>
     </div>
-    <!-- Main page content-->    
+    <!-- Main page content-->
 </main>
 @push('css')
     <link href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
@@ -71,21 +71,21 @@
     <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
     <script>
-      
+
         $(document).ready(function() {
             const capitalize = (s) => {
                 if (typeof s !== 'string') return ''
                 return s.charAt(0).toUpperCase() + s.slice(1)
             }
             var dataTable =  $('#dataTable').DataTable({
-                
+
                 "processing": true,
                 "serverSide": true,
                 'searching': true,
                 "ajax": {
                     "url": "{{ route('aonePaymentData') }}",
                     "data": function(data){
-                     
+
                         data['sort_field'] = data.columns[data.order[0].column].name;
                         data['sort_dir'] =  data.order[0].dir;
                         data['search'] = data.search.value;
@@ -95,17 +95,17 @@
 
                         var filter_status = $('#filter_status').val();
                         data.filter_status = filter_status;
-                        
+
                         var filter_verification = $('#filter_verification').val();
                         data.filter_verification = filter_verification;
                     }
-                   
+
                 },
                 "fnDrawCallback": function( oSettings ) {
                     feather.replace();
                     $('[data-toggle="popover"]').popover();
                 },
-                
+
                 "columns": [
                     { "data": "id", "name":'id', "orderable": true },
                     { "data": "first_name", "name":'first_name', "orderable": true },
@@ -130,7 +130,7 @@
 	            	    // return capitalize(str)
                         return '<div style="font-size:12px;" class="badge '+colorClass+' badge-pill">'+str+'</div>';
 	                }},
-                    { "data": "id", "name":'edit', "orderable": false, "sClass": "content-middel selectOff", 
+                    { "data": "id", "name":'edit', "orderable": false, "sClass": "content-middel selectOff",
 	            	    render: function ( data, type, row, meta) {
 	            	    return '<a href="javascript:;" edit_item_id="'+row.id+'" class="item_edit"><button class="btn btn-datatable btn-icon btn-transparent-dark"><i data-feather="edit"></i></button></a>';
 	                }},
